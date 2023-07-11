2deb3653d66ccc556596b9bd68341c5bc275d704
matrixmm <thetnaing078106@gmail.com>
2023-07-11 22:34:19 +0630

    Update Shipping.tsx

--- components/product/Shipping.tsx
+++ components/product/Shipping.tsx
@@ -4,9 +4,9 @@ import {faPhoneVolume} from '@fortawesome/free-solid-svg-icons/faPhoneVolume';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const shippingInfo = [
-	{id: 1, icon: faCalendarAlt, text: 'Extended warranty for 30 days.'},
-	{id: 2, icon: faUndo, text: 'Changed your mind? No problem!'},
-	{id: 3, icon: faPhoneVolume, text: 'Customer support line'},
+	{id: 1, icon: faCalendarAlt, text: 'Extended warranty for 10 days.'},
+	{id: 2, icon: faUndo, text: 'If you want to exchange second hand phones purchased from the shop within (1) week, you can exchange them with 10% off.If you want to resell, you can buy it again with 15% off.(Not including new phones)'},
+	{id: 3, icon: faPhoneVolume, text: 'Customer support line 09 5343769,09 984250942'},
 ];
 
 export default function ProductShipping() {
